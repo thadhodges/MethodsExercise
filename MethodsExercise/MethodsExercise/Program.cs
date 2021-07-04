e8bd416658546abd96373da9cfaf56cee3439ea5
Thad <thadisfree@gmail.com>
2021-07-03 21:55:46 -0500

    first addition method

--- MethodsExercise/MethodsExercise/Program.cs
+++ MethodsExercise/MethodsExercise/Program.cs
@@ -4,9 +4,15 @@ namespace MethodsExercise
 {
     class Program
     {
+        public static int Addition(int num1, int num2)
+        {
+            int addition = num1 + num2;
+            return addition;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Addition(12, 34);
+            Addition(4, 9);
         }
     }
 }
