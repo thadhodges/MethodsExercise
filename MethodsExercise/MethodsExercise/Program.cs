b3f6eb9b57f9b7111e86d8824d951526b547cfd5
Thad <thadisfree@gmail.com>
2021-07-03 22:38:34 -0500

    added multiple methods

--- MethodsExercise/MethodsExercise/Program.cs
+++ MethodsExercise/MethodsExercise/Program.cs
@@ -6,13 +6,63 @@ namespace MethodsExercise
     {
         public static int Addition(int num1, int num2)
         {
-            int addition = num1 + num2;
-            return addition;
+            return num1 + num2;
+            
+        }
+        public static int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Mulitiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Square(int number)
+        {
+            return number * number;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2; 
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        public static bool IsEven(int number)
+        {
+            if ((number % 2) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         static void Main(string[] args)
         {
-            Addition(12, 34);
-            Addition(4, 9);
+            Console.WriteLine(Addition(12, -34));
+            Console.WriteLine(Addition(4, 9));
+            Console.WriteLine(Subtraction(234, 43));
+            Console.WriteLine(Subtraction(-345567, 22345));
+            Console.WriteLine(Mulitiply(2, 90));
+            Console.WriteLine(Mulitiply(456, 33457));
+            Console.WriteLine(Square(45));
+            Console.WriteLine(Square(-123456));
+            Console.WriteLine(Divide(123456789, 1234));
+            Console.WriteLine(Divide(45, 9));
+            Console.WriteLine(Modulus(123456789, 1234));
+            Console.WriteLine(Modulus(45, 9));
+            Console.WriteLine(IsEven(345));
+
+
+            
         }
     }
 }
